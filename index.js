92ef7676cc134ac3a8e7e106278f4d5e66db5381
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-05-16 20:44:25 +0600

    implement jwt token system

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -26,6 +27,26 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+
+  const authorization = req.headers.authorization
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "Unauthoried access" })
+  }
+
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRECT, (err, decoded) => {
+    if(err){
+      return res.status(403).send({error:true, message: "unautoried access"})
+    }
+
+    req.decoded = decoded
+    next()
+  });
+
+  console.log(token);
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -35,6 +56,19 @@ async function run() {
     const serviceCollection = client.db("carDoctorDB").collection("services")
     const bookingCollection = client.db("carDoctorDB").collection("bookings")
 
+    // jwt
+    app.post('/token', (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRECT, {
+        expiresIn: "1h"
+      })
+
+      const result = { token }
+
+      res.send(JSON.stringify(result))
+    })
+
+    // Service Route
     app.get('/services', async (req, res) => {
 
       const result = await serviceCollection.find().toArray()
@@ -64,7 +98,10 @@ async function run() {
       res.send(service)
     })
 
-    app.get('/bookings', async (req, res) => {
+    app.get('/bookings', verifyJWT, async (req, res) => {
+
+      // const user = req.headers.authorization
+      // console.log(user);
 
       let query = {}
       if (req.query?.email) {
@@ -74,6 +111,7 @@ async function run() {
       }
 
       const result = await bookingCollection.find(query).toArray()
+      console.log(result);
       res.send(result)
     })
 
